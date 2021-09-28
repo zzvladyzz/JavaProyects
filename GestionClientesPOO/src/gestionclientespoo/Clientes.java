a66c97e6fd22cabf60cbd43db33f168b864dc398
vlady <vladimir_1913@hotmail.com>
2021-09-28 22:23:02 +0200

    9/28/2021

---
+++ GestionClientesPOO/src/gestionclientespoo/Clientes.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionclientespoo;
+
+/**
+ *
+ * @author vladi
+ */
+public class Clientes {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getNombreCompleto() {
+        return nombre+" "+apellido;
+    }
+
+}
---
+++ GestionClientesPOO/src/gestionclientespoo/GestionClientesPOO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestionclientespoo;
+
+/**
+ *
+ * @author vladi
+ */
+public class GestionClientesPOO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // iniciamos la ventana
+        Panel window =new Panel();
+        window.show();
+    }
+    
+}
